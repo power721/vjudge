1b8dae3cefec3f0ee42ed2c87a09b662d3a01989
power0721@gmail.com <power0721@gmail.com@b9ef930c-4288-2b95-b924-2bf97a46c405>
2013-08-28 14:27:40 +0000

    add logger for controller

--- src/vjudge/action/BaseController.java
+++ src/vjudge/action/BaseController.java
@@ -1,8 +1,14 @@
 package vjudge.action;
 
 import com.jfinal.core.Controller;
+import com.jfinal.log.Logger;
 
 public class BaseController extends Controller
 {
-
+	protected final Logger log = Logger.getLogger(getClass());
+	
+	public Logger getLog()
+	{
+		return log;
+	}
 }
